45c19c3f0ff6664e4d92068ac0ffc9026c2f1185
WestonWink <weston.g.winkeljohn-1@ou.edu>
2023-11-05 20:50:06 -0600

    fin

--- hello_django/settings.py
+++ hello_django/settings.py
@@ -21,11 +21,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', default='foo')
+SECRET_KEY = os.environ.get("SECRET_KEY", "secret_key")
 
-DEBUG = int(os.environ.get('DEBUG', default=0))
+DEBUG = os.environ.get("DEBUG", True)
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'rocky-cove-87571.herokuapp.com']
+ALLOWED_HOSTS = [".onrender.com"]
 
 
 # Application definition
